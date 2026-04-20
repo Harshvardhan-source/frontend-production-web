dc4bc433e0a499f6c15c44bc85bb884deb4fa84a
Harshavardhan-source <ravindraacharya0512@gmail.com>
2026-04-20 13:52:10 +0530

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -129,7 +129,7 @@ export default function App() {
           <Route path="/voters"         element={<Protected><VoterSearch /></Protected>} />
           <Route path="/sir"            element={<Protected><SIR /></Protected>} />
           <Route path="/admin"          element={<Protected><AdminPanel /></Protected>} />
-          <Route path="/swot"           element={<ProtectedRoute><Swot /></ProtectedRoute>} />
+          
           <Route path="*"               element={<Navigate to="/" replace />} />
 
         </Routes>
